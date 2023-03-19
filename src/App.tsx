bc6ffeca9745a099c7cef6533cb673c94d1a99b7
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-03-19 12:20:48 -0400

    adding connector lines

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,33 @@ function Node({ node }: { node: Node }) {
       <div style={{ gridArea: "value" }}>{node.value}</div>
       {node.left && (
         <div style={{ gridArea: "left" }}>
+          <svg
+            className="line"
+            viewBox="0 0 50 50"
+          >
+            <line
+              x1="0"
+              y1="25"
+              x2="25"
+              y2="0"
+            />
+          </svg>
           <Node node={node.left} />
         </div>
       )}
       {node.right && (
         <div style={{ gridArea: "right" }}>
+          <svg
+            className="line"
+            viewBox="0 0 50 50"
+          >
+            <line
+              x1="25"
+              y1="0"
+              x2="50"
+              y2="25"
+            />
+          </svg>
           <Node node={node.right} />
         </div>
       )}
@@ -41,7 +63,7 @@ function App() {
     left: {
       value: 1,
       left: { value: -3, right: { value: -1 } },
-      right: { value: 2, right: { value: 3 } },
+      right: { value: 3, left: { value: 2 }, right: { value: 4 } },
     },
     right: {
       value: 8,
